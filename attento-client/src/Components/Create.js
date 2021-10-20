97ccad3005623a8ddebc6e632d6f9d2f06bddd15
Tushar Ojha <tusharojha2001@gmail.com>
2021-10-20 22:34:04 +0530

    added check for class name

--- attento-client/src/Components/Create.js
+++ attento-client/src/Components/Create.js
@@ -14,17 +14,19 @@ const Create = ({ onCreate }) => {
             <div className="input-btn">
                 <input type="text" value={lecName} onChange={(c) => setLecName(c.target.value)} className="new-lec" placeholder="Enter Lecture Name    ex. CSE1/lec2/March/14" />
                 <button onClick={() => {// Lec create krna padega...
-                    var collectionRef = collection(db, 'classes');
-                    console.log(document.id);
-                    addDoc(collectionRef, {
-                        name: lecName,
-                        live: true,
-                        createdOn: Timestamp.now(),
-                        attendees: {}
-                    }).then((docRef) => {
-                        console.log(docRef);
-                        onCreate(lecName, docRef.id);
-                    });
+                    if (lecName !== "") {
+                        var collectionRef = collection(db, 'classes');
+                        console.log(document.id);
+                        addDoc(collectionRef, {
+                            name: lecName,
+                            live: true,
+                            createdOn: Timestamp.now(),
+                            attendees: {}
+                        }).then((docRef) => {
+                            console.log(docRef);
+                            onCreate(lecName, docRef.id);
+                        });
+                    }
                 }}> Create Class</button>
 
             </div>
